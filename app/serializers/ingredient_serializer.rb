e9f73be5c62c8e8d6e6efbdb8349110220e3adc4
Jessica Joseph <jessicaajosephh@gmail.com>
2021-08-08 10:19:02 -0400

    commented out the ingredient serializer

--- app/serializers/ingredient_serializer.rb
+++ app/serializers/ingredient_serializer.rb
@@ -1,4 +1,4 @@
 class IngredientSerializer
-    include FastJsonapi::ObjectSerializer 
-    attributes :name, :cocktail_id 
+    # include FastJsonapi::ObjectSerializer 
+    # attributes :name, :cocktail_id 
 end
