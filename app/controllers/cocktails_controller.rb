37353304e091ee1f2d29ebb23bd95f04926ab847
Jessica Joseph <jessicaajosephh@gmail.com>
2021-08-08 10:02:37 -0400

    created an index and create method in the cockatils controller so that the browse will  be able to display all cocktails as json

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,17 @@
 class CocktailsController < ApplicationController
+
+    def index 
+        cocktails = Cocktail.all 
+        render json: CocktailSerializer.new(cocktails)
+    end
+
+    def create 
+        cocktail = Cocktail.new(cocktail_params)
+            if cocktail.save 
+                render json: cocktail, status: :accepted
+            else 
+                render json: {errors: cocktail.errors.full_messages}, status: :unprocessable_entity
+            end
+    end
+
 end
