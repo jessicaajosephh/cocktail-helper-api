b08f25dddc66e79b427585bef5d42083ed8186fa
Jessica Joseph <jessicaajosephh@gmail.com>
2021-08-08 10:08:44 -0400

    created an index and create method in the ingredients controller

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,17 @@
 class IngredientsController < ApplicationController
+
+    def index 
+        ingredients = Ingredient.all 
+        render json: IngredientSerializer.new(ingredients)
+    end
+
+    def create 
+        ingredient = Ingredient.new(ingredient_params)
+            if ingredient.save 
+                render :ingredient, status: :accepted 
+            else 
+                render json: {errors: ingredient.errors.full_messages}, status: :unprocessable_entity
+            end
+    end
+
 end
