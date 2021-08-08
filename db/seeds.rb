081b76b06f34715e2a877653630200eded1db51b
Jessica Joseph <jessicaajosephh@gmail.com>
2021-08-08 09:57:07 -0400

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cocktail.destroy_all
+Ingredient.destroy_all 
+
+mimosa = Cocktail.create(name: "Mimosa", image: "https://cdn.loveandlemons.com/wp-content/uploads/2019/12/mimosa-1080x1080.jpg", instructions: "Pour the sparkling wine into the Champagne flutes, filling each glass about half full. Pour the orange juice on top to fill each glass and serve")
+
+prosecco = Ingredient.create(name: "Prosecco - 2.5 oz", cockatil_id: mimosa.id)
+orange_juice = Ingredient.create(name: "Orange Juice - 2.5 oz", cocktail_id: mimosa.id)
